a878e16dec1c957b42f8e0beb17ea0a94acdf6fb
Ani Gevorgyan <anya.gevorgyan.2014@mail.ru>
2023-01-14 01:00:41 +0300

    Done

---
+++ Homework/Test1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символам
+
+Console.Clear();
+
+string[] oldarray = new string[5] { "Soft", "GeekBrains", "Yes", "24", "HardWork" };
+string[] newarray = new string[oldarray.Length];
+
+void NewShortArray(string[] oldarray, string[] newarray)
+{
+    int index = 0;
+    for (int i = 0; i < oldarray.Length; i++)
+    {
+        if (oldarray[i].Length <= 3)
+        {
+            newarray[index] = oldarray[i];
+            index++;
+        }
+    }
+
+}
+
+void PrintArray(string[] shortarray)
+{
+    for (int i = 0; i < shortarray.Length; i++)
+    {
+    Console.Write($"{shortarray[i]} ");
+    }
+    //Console.WriteLine();
+}
+
+
+NewShortArray(oldarray, newarray);
+PrintArray(newarray);
